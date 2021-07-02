d6d5f4df6213b5f8465bb5cfa6990940aa46f24b
jordans1882 <jordans1882@gmail.com>
2021-07-02 16:56:04 -0600

    Add counsel-gtags

--- init.el
+++ init.el
@@ -142,6 +142,16 @@
 (use-package company-lsp
   :straight t
   :commands company-lsp)
+(use-package counsel-gtags
+  :straight t
+  :config
+    (add-hook 'c-mode-hook 'counsel-gtags-mode)
+    (add-hook 'c++-mode-hook 'counsel-gtags-mode)
+    (with-eval-after-load 'counsel-gtags
+        (define-key counsel-gtags-mode-map (kbd "M-t") 'counsel-gtags-find-definition)
+        (define-key counsel-gtags-mode-map (kbd "M-r") 'counsel-gtags-find-reference)
+        (define-key counsel-gtags-mode-map (kbd "M-s") 'counsel-gtags-find-symbol)
+        (define-key counsel-gtags-mode-map (kbd "M-,") 'counsel-gtags-go-backward)))
 (use-package conda
   :straight t
   :init
